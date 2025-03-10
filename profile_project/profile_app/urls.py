4d06095fa47f17db7190b7abff7c1435bf12696e
Tubo Jesu <jesulayomioguntubo@gmail.com>
2025-03-10 07:02:06 -0400

    Added a new project to the existing ones

---
+++ profile_project/profile_app/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+
+from . import views
+
+
+urlpatterns = {
+    path('', views.home, name="home"),
+    path('goals/', views.goals, name="goals"),
+    path('hobbies/', views.hobbies, name="hobbies"),
+}
